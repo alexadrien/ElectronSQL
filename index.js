ee7cfe27ab6ca8f7da374ec4474d18743890be2e
Acube <alex.adrien.auger@gmail.com>
2018-03-30 16:39:08 +0200

    Add function to parse metadata

--- index.js
+++ index.js
@@ -88,3 +88,29 @@ app.on('activate', () => {
     createWindow()
   }
 })
+
+function readMetaFromFile(filePath){
+  const fileBuf = fs.readFileSync(filePath)
+  const chunkNameBuf = fileBuf.slice(0,4)
+  const chunkSizeBuf = fileBuf.slice(4,8)
+  const chunkFormNameBuf = fileBuf.slice(8,12)
+  const chunkDataBuf = fileBuf.slice(12)
+
+  var currentIndex = 0
+  var ckid = -1
+  var cksz = -1
+  var ckdata
+  while(currentIndex < chunkDataBuf.length){
+    ckid = chunkDataBuf.slice(currentIndex, currentIndex+4).toString()
+    currentIndex += 4
+    cksz = chunkDataBuf.slice(currentIndex, currentIndex+4).readUInt32LE()
+    currentIndex += 4
+    ckdata = chunkDataBuf.slice(currentIndex, currentIndex+cksz)
+    currentIndex += cksz
+    if(ckid == "iXML"){
+      var parsedxml = {}
+      xmlParseString(ckdata.toString(), function(err, result){ parsedxml = result })
+      return parsedxml
+    }
+  }
+}
