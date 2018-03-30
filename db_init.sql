d0ab6c60dbb8ca1f95d7e40b94b67424f581c21e
Acube <alex.adrien.auger@gmail.com>
2018-03-30 09:46:05 +0200

    Edit file with a new column and remove sample data insertion

--- db_init.sql
+++ db_init.sql
@@ -1,9 +1,5 @@
 CREATE TABLE `files` (
   `filename` varchar DEFAULT NULL,
   `type` varchar DEFAULT NULL,
-  `created` varchar DEFAULT NULL
+  `completepath` varchar DEFAULT NULL
 );
-INSERT INTO `files` VALUES ('ok.json', 'json', '2018-04-01');
-INSERT INTO `files` VALUES ('ok1.txt', 'Text file', '2018-05-01');
-INSERT INTO `files` VALUES ('ok2.wav', 'Machin Wav', '2018-03-01');
-INSERT INTO `files` VALUES ('ok3.mp3', 'MP3', '2018-04-02');
