398fea9d267b16f559fae30362ac740f0c82adf9
Acube <alex.adrien.auger@gmail.com>
2018-04-02 13:06:33 +0200

    Edit sql init to allow metadata to be stored

--- db_init.sql
+++ db_init.sql
@@ -1,5 +1,7 @@
 CREATE TABLE `files` (
   `filename` varchar DEFAULT NULL,
   `type` varchar DEFAULT NULL,
-  `completepath` varchar DEFAULT NULL
+  `completepath` varchar DEFAULT NULL,
+  `metaname` varchar DEFAULT NULL,
+  `metavalue` varchar DEFAULT NULL
 );
