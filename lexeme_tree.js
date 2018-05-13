7314239f5d796abe36c9b89293b30a06c72c4225
C-rror <36012686+C-rror@users.noreply.github.com>
2018-05-13 18:54:30 +0200

    new lexeme_tree.js
    Updated the lexeme_tree with new nodes and children :
    "dans_le_lieu"
        "humain"
            "presence" | "quantite" | "voix" | "caractere"
        "non-humain"
            "machine" | "electricite"

--- lexeme_tree.js
+++ lexeme_tree.js
@@ -1,13 +1,19 @@
-{
-	"id" : "interieur",
+/*
+{ "id" : "object_name",
 	"children" : [
-		{
-			"id" : "acoustique",
+		{ "id" : "child1", "values" : ["val1", "val2"] },
+		{ "id" : "child2", "values" : ["val1", "val2"] },
+	]
+}
+*/
+
+{ "id" : "interieur",
+	"children" : [
+		{ "id" : "acoustique",
 			"children" : [
 				{ "id" : "taille", "values" : ["petit", "grand"] },
 				{ "id" : "stereo", "values" : ["none", "large"]},
-				{
-					"id" : "reverberation",
+				{ "id" : "reverberation",
 					"children" : [
 						{ "id" : "TR60", "values" : ["mat", "reverberant"] },
 						{ "id" : "couleur", "values" : ["clair", "sombre"] }
@@ -15,19 +21,31 @@
 				}
 			]
 		},
-		{
-			"id" : "source_sonore",
+		{ "id" : "source_sonore",
 			"children" : [
-				{
-					"id" : "dans_le_lieu",
+				{ "id" : "dans_le_lieu",
 					"children" : [
-						{ "id" : "caractere", "values" : ["calme", "agite"] },
-						{ "id" : "voix", "values" : ["none", "voix"] },
-						{ "id" : "densite", "values" : ["vide", "foule"] }
+//						{ "id" : "caractere", "values" : ["calme", "agite"] },
+//						{ "id" : "voix", "values" : ["none", "voix"] },
+//						{ "id" : "densite", "values" : ["vide", "foule"] },
+						{ "id" : "humain",
+							"children" : [
+								{ "id" : "presence", "values" : ["none", "presence"] },
+								{ "id" : "quantite", "values" : ["groupe", "foule"] },
+								{ "id" : "voix", "values" : ["chuchotement", "voix", "cri"] },
+								{ "id" : "caractere", "values" : ["calme", "activite", "agite"] },
+							]
+						},
+						{ "id" : "non-humain",
+							"children" : [
+								{ "id" : "machine", "values" : ["machine", "moteur", "ventilateur"] },
+								{ "id" : "electricite", "values" : ["hum", "buzz", "neon", "induction"] },
+							]
+						}
+
 					]
 				},
-				{
-					"id" : "hors_du_lieu",
+				{ "id" : "hors_du_lieu",
 					"children" : [
 						{ "id" : "avion", "values" : ["none", "avion"] },
 						{ "id" : "circulation", "values" : ["none", "avion"] },
