f4f12b20b9fefd0a0954562675f47fae18a27544
Alex-Adrien <alex.adrien.auger@gmail.com>
2018-05-15 06:48:38 +0200

    edit db

--- db_init.sql
+++ db_init.sql
@@ -1,7 +1,5 @@
 CREATE TABLE `files` (
   `filename` varchar DEFAULT NULL,
-  `type` varchar DEFAULT NULL,
-  `completepath` varchar DEFAULT NULL,
-  `metaname` varchar DEFAULT NULL,
-  `metavalue` varchar DEFAULT NULL
+  `purefilename` varchar DEFAULT NULL,
+  `completepath` varchar DEFAULT NULL
 );
