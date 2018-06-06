8f7c4748917899e890893c82610847c31b532072
AlexAdrienAuger <alex.adrien.auger@gmail.com>
2018-06-06 17:48:28 +0200

    remove accents

--- functions/preprocessing/cleaning.js
+++ functions/preprocessing/cleaning.js
@@ -1,6 +1,8 @@
+import sansAccent from 'sans-accent';
+
 export default function(filename, equivalences) {
   return Object.keys(equivalences).reduce((accumulator, currentValue) => {
     const regex = RegExp("\\b" + currentValue + "\\b", "g");
     return accumulator.replace(regex, equivalences[currentValue]);
-  }, filename.toLowerCase());
+  }, sansAccent()(filename.toLowerCase()));
 }
